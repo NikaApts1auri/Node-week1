ce4fb96e94a8f86f0bcc3a86f917fe91d0d5d090
NikaApts1auri <nika.aptsiauri77@gmail.com>
2025-04-26 19:56:32 +0400

    learn slugify and nodemon and some details

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 import fs from "fs";
 import http from "http";
+import url from "url";
+import slugify from "slugify";
 
-fs.writeFileSync(
-  "message.txt",
-  "this is new line"
-);
-console.log(" ტექსტი წარმატებით დაემატა!");
+const posts = fs.readFileSync("data.json", "utf8");
+const str = slugify("Hello World", { lower: true });
+console.log(str);
 
-fs.unlinkSync("message.txt");
-console.log("ფაილი წარმატებით წაიშალა!");
+fs.writeFileSync("message.txt", "this is new line");
+console.log("ტექსტი წარმატებით დაემატა!");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "content-type": "text/plain",
-  });
-  if (req.url === "/") {
-    res.end("Home Page");
-  } else if (req.url === "/about") {
-    res.end("About Page");
-  } else {
-    res.writeHead(404, {
+  const { id } = url.parse(req.url, true).query;
+  const { pathname } = url.parse(req.url, true);
+  console.log("id aris :", id);
+
+  if (pathname === "/") {
+    res.writeHead(200, {
       "Content-Type": "text/plain",
-    }); // ამას იმიტომ ვიმეორებთ რომ 200 სტატუს დააბრუნებს და იქნება Not found 404 bad practive :))
-    res.end("404 Not Found");
+    });
+    res.end("Home Page");
+  } else if (pathname === "/about") {
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
+    });
+    res.end("About Page");
+  } else if (pathname === "/posts") {
+    if (id) {
+      const post = JSON.parse(posts).find((post) => post.id === Number(id));
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify(post));
+      return;
+    }
+
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(posts);
   }
-  res.end("Welcome to my Node.js server!");
 });
 
 server.listen(3000, "localhost", () => {
-  console.log("server is running");
+  console.log("server is running http://localhost:3000");
 });
